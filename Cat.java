272e2de90183e60c4499bfe697580f42aec81d45
Arash Sa <101516570+Arashsa31@users.noreply.github.com>
2023-07-20 17:14:43 -0400

    Add files via upload

---
+++ Cat.java
@@ -0,0 +1,13 @@
+package LabProgram214;
+
+public class Cat extends Pet {
+   private String breed;
+
+   public void setBreed(String userBreed) {
+      breed = userBreed;
+   }
+
+   public String getBreed() {
+      return breed;
+   }
+}
